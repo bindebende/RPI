4ce1ff6bff2c982fa3eb544e9b082d51615fbd4f
Hoffmann Bendegúz <hoffmann.bende@gmail.com>
2020-06-06 15:17:40 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 # stop if error was raised https://stackoverflow.com/a/2871034/4109273
-set -euxo pipefail
+set -eux
 
 echo "start"
+
+# add this file to 
+
+# exectue commands which requires backup
+# if restart not happened
+# write to a log file 
